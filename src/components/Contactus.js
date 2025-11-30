efc8a429cf62f66d8fa2c5c23724870fd399be6c
MayuMehta <mayushar8434@gmail.com>
2025-12-01 00:29:28 +0530

    Contactus update

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -3,27 +3,25 @@ import React from "react";
 function Contactus(props) {
   return (
     <>
-      <div className={`container text-${props.textColor} min-vh-100 py-4 text-center`}>
-        <div>
-          
-        </div>
-        <div>
+      <div className={`container d-flex justify-content-center text-${props.textColor} min-vh-100 py-4`}>
+        <div className="border p-4 h-100">
+          <h2>Contact Us</h2>
           <p>if you have any query, fill this form. We'll get back to you.</p>
           <form action="" className="py-3">
             <div>
               <label htmlFor="name" className="fw-bold">Name:</label>
               <br />
-              <input type="text" className="w-25" />
+              <input type="text" className="w-100"/>
             </div>
             <div>
               <label htmlFor="email" className="fw-bold">Email:</label>
               <br />
-              <input type="email" className="w-25" />
+              <input type="email" className="w-100" />
             </div>
             <div>
               <label htmlFor="message" className="fw-bold">Message:</label>
               <br />
-              <textarea name="message" id="message" className="w-25" rows={5}></textarea>
+              <textarea name="message" id="message" className="w-100" rows={5}></textarea>
             </div>
             <input type="submit" value="Submit" className="btn btn-success" />
           </form>
